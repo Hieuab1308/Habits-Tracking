38ee961b6076fc5ae1d32e733ab3551c8f272ebc
c4ptur3-th3-fl4g <22010222@st.phenikaa-uni.edu.vn>
2024-09-30 17:04:20 +0700

    Refactor admin class and add getters and setters
    This commit refactors the admin class in the src/Class/admin.java file. It adds getters and setters for the id, email, and matkhau properties. This change improves the code structure and encapsulation.

---
+++ src/Class/admin.java
@@ -0,0 +1,41 @@
+package Class;
+import java.io.Serializable;
+
+public class admin implements Serializable {
+    private String id;            //tên đăng nhập
+//    private int id;                 // id 
+    private String email;           // email
+    private String matkhau;         //mật khẩu đăng nhập
+    private boolean role;            // vai trò (admin và user)  
+
+    public admin(){}
+    public admin( String id, String email, String matkhau, boolean role) {
+        this.id = id;
+
+        this.email = email;
+        this.matkhau = matkhau;
+        this.role = role;
+    }
+}
+
+public String getId() {
+    return id;
+}
+public void setId(String id) {
+    this.id = id;
+} 
+public String getEmail() {
+    return email;
+}
+public void setEmail(String email) {
+    this.email = email;
+}
+public String getmatkhau() {
+    return matkhau;
+}
+public void setPass(String matkhau) {
+    this.matkhau = matkhau;
+}
+public boolean getRole(){
+    return role;
+}
