b14ac4b58dbf0587b49ae0010c7730a6431bf3bb
c4ptur3-th3-fl4g <22010222@st.phenikaa-uni.edu.vn>
2024-10-02 11:09:05 +0700

    Refactor deleteAccount method in taikhoandelete.java

--- src/manageSQL/taikhoandelete.java
+++ src/manageSQL/taikhoandelete.java
@@ -25,27 +25,27 @@ public class taikhoandelete {
         return null; // Return null if no role is found or an exception occurs
     }
 
-    public boolean deleteAccount(String currentEmail, String targetEmail) {
-        String currentUserRole = getUserRole(currentEmail);
-        if (currentUserRole == null) {
-            System.err.println("Cannot find role of current user");
-            return false;
-        }
-        if (currentUserRole.equals("TRUE")) {
-            return deleteAccountFromDatabase(targetEmail);
-        }
-        else if (currentUserRole.equals("FALSE")) {
-            if (currentEmail.equals(targetEmail)) {
-                return deleteAccountFromDatabase(currentEmail);
-            }
-            else {
-                System.out.println("You do not have permission to delete other users");
-                return false;
-            }
-        }
-        System.out.println("Error: Unknown role");
-        return false;
-    }
+    // public boolean deleteAccount(String currentEmail, String targetEmail) {
+    //     String currentUserRole = getUserRole(currentEmail);
+    //     if (currentUserRole == null) {
+    //         System.err.println("Cannot find role of current user");
+    //         return false;
+    //     }
+    //     if (currentUserRole.equals("TRUE")) {
+    //         return deleteAccountFromDatabase(targetEmail);
+    //     }
+    //     else if (currentUserRole.equals("FALSE")) {
+    //         if (currentEmail.equals(targetEmail)) {
+    //             return deleteAccountFromDatabase(currentEmail);
+    //         }
+    //         else {
+    //             System.out.println("You do not have permission to delete other users");
+    //             return false;
+    //         }
+    //     }
+    //     System.out.println("Error: Unknown role");
+    //     return false;
+    // }
 
     private boolean deleteAccountFromDatabase(String targetEmail) {
         String deleteQuery = "DELETE FROM users WHERE email = ?";
